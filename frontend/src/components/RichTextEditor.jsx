bdb0768db19c3183d653a2047b191036206f7a83
Zaid Kamil <kamilzaid53@gmail.com>
2026-03-09 03:00:49 +0530

    Add a custom wrapper to rich text editor makeing it usable in dark mode

--- frontend/src/components/RichTextEditor.jsx
+++ frontend/src/components/RichTextEditor.jsx
@@ -4,13 +4,15 @@ import "react-quill/dist/quill.snow.css";
 
 function RichTextEditor({ value, onChange, placeholder = "" }) {
   return (
-    <ReactQuill
-      value={value}
-      onChange={onChange}
-      theme="snow"
-      placeholder={placeholder}
-      className="mb-4"
-    />
+    <div className="quill-Custom-Wrapper mb-6 relative z-0">
+      <ReactQuill
+        value={value}
+        onChange={onChange}
+        theme="snow"
+        placeholder={placeholder}
+        className="bg-white dark:bg-gray-800 text-gray-900 dark:text-white rounded-lg shadow-sm border border-gray-300 dark:border-gray-600 transition-colors"
+      />
+    </div>
   );
 }
 
